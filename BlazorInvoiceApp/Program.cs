113b695185b4fa4fdf8d363bf6e50e29e3b83b03
nirzaf <mfmfazrin1986@gmail.com>
2024-02-11 19:52:30 +0300

    🎨 style: Remove unnecessary blank lines and comments in Program.cs

--- BlazorInvoiceApp/Program.cs
+++ BlazorInvoiceApp/Program.cs
@@ -14,8 +14,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
+        
         var connectionString 
             = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
         builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
@@ -37,8 +36,6 @@ public class Program
         IMapper mapper = mapperConfig.CreateMapper();
         builder.Services.AddSingleton(mapper);
 
-
-
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -49,22 +46,16 @@ public class Program
         else
         {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
         app.UseHttpsRedirection();
-
         app.UseStaticFiles();
-
         app.UseRouting();
-
         app.UseAuthorization();
-
         app.MapControllers();
         app.MapBlazorHub();
         app.MapFallbackToPage("/_Host");
-
         app.Run();
     }
 }
